4f4b597d6b1bd4f6b42b0bf068a9cd7c97619125
tonyhew <916390127@qq.com>
2023-08-03 11:27:53 +0800

    增加 Alert 组件的文档及修正 Alert 标题的样式等

---
+++ src/components/Alert/alert.stories.tsx
@@ -0,0 +1,90 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import Alert from './alert'
+
+const meta: Meta<typeof Alert> = {
+  title: 'T-UI/Alert',
+  component: Alert,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered',
+  },
+  argTypes: {
+    title: {
+      type: 'string',
+      description: `Alert Title`,
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'Alert' } 
+      }
+    },
+    description: {
+      type: 'string',
+      description: `Alert 的描述`,
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'this is description about alert componenet' } 
+      }
+    },
+    type: {
+      type: 'string',
+      options: ['success', 'default', 'danger', 'warning'],
+      control: 'select',
+      description: '不同类型的 Alert',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'primary' }
+      }
+    },
+    onClose: {
+      type: 'function',
+      description: '关闭alert时触发的事件',
+      table: {
+        type: { summary: '() => void' },
+      }
+    },
+    closable: {
+      type: 'boolean',
+      description: '是否能关闭 Alert',
+      defaultValue: true,
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'true' }
+      }
+    },
+    className: {
+      type: 'string',
+    },
+    icon: {
+      type: 'string'
+    }
+  }
+}
+
+export default meta
+type Story = StoryObj<typeof Alert>
+
+export const AlertDoc: Story = {
+  name: 'Alert',
+  args: {
+    title: 'Alert',
+    description: '', // 描述
+    type: 'default',
+  },
+}
+
+export const AlertWithDescription: Story = {
+  name: '带描述的 Alert',
+  args: {
+    title: 'Alert',
+    description: 'this is describe about alert component',
+  },
+}
+
+export const AlertWithDifferentType: Story = {
+  name: '不同类型的 Alert',
+  args: {
+    title: 'Different Type Alert',
+    description: 'these are different type of the alert',
+    type: 'danger',
+  },
+}
--- src/components/Button/button.stories.tsx
+++ src/components/Button/button.stories.tsx
@@ -4,7 +4,7 @@ import Button from './button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta: Meta<typeof Button> = {
-  title: 'T-UI/按钮',
+  title: 'T-UI/Button',
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
